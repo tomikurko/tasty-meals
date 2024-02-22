e7fc207fb1dbd6a7db3aa9cd668667b0c577799e
Tomi Kurko <tomi.kurko@iki.fi>
2024-02-22 14:24:21 +0200

    Frontend: Get new random recipe on each visit to Explore page

--- frontend/lib/screens/explore_screen.dart
+++ frontend/lib/screens/explore_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tastymeals/providers/random_recipe_future_provider.dart';
 import 'package:tastymeals/widgets/explore_widget.dart';
 import 'package:tastymeals/widgets/screen_frame.dart';
 
-class ExploreScreen extends StatelessWidget {
+class ExploreScreen extends ConsumerWidget {
+  bool refreshed;
+
+  ExploreScreen() : refreshed = false;
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    if (!refreshed) {
+      ref.refresh(randomRecipeFutureProvider);
+      refreshed = true;
+    }
+
     return buildScreenFrame(context, 2,
         Padding(padding: const EdgeInsets.all(18.0), child: ExploreWidget()));
   }
